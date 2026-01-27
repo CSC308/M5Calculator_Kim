2e2349df30a72692e530eb4ef351b071313e069d
Eun Mi Kim <koreaeunmi@gmail.com>
2026-01-27 11:24:26 -0500

    Initial Commit

---
+++ M2Calculator_Kim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2Calculator_Kim
+//
+//  Created by Eun Mi Kim on 1/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
